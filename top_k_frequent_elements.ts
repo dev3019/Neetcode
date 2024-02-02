b824cae6d0dd1126a5590750432fba380363dc43
Divyansh Tangri <divyansh@wynd.network>
2024-02-02 00:51:26 -0800

    removed ts

--- top_k_frequent_elements.ts
+++
@@ -1,8 +0,0 @@
-function topKFrequent(nums: number[], k: number): number[] {
-  const map: Map<number, number> = new Map()
-  for (const num of nums) map.set(num, (map.get(num)??0)+1)
-  const sortedArray = Array.from(new Set(nums)).sort((a,b)=>map[b]-map[a])
-  return sortedArray.splice(0, k);
-};
-
-console.log(topKFrequent([1,1,1,2,2,3], 2))
