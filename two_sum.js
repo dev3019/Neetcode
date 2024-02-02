2e77ff7fc7bd0b0aa5c2108a71c7b428a55810d5
Divyansh Tangri <divyansh@wynd.network>
2024-02-02 00:52:16 -0800

    .

--- two_sum.js
+++ two_sum.js
@@ -1,16 +1,19 @@
-function twoSum(nums, target) {
-    var _a;
-    var map = new Map();
-    var array = [];
-    for (var i in nums) {
-        var complement = target - nums[i];
-        if (map.has(complement)) {
-            array = [(_a = map.get(complement)) !== null && _a !== void 0 ? _a : 0, parseInt(i, 10)];
-            break;
-        }
-        map.set(nums[i], parseInt(i, 10));
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map()
+    let array = []
+    for(const i in nums){
+      const complement = target - nums[i]
+      if(map.has(complement)){
+        array = [map.get(complement) ?? 0, parseInt(i, 10)]
+        break;
+      }
+      map.set(nums[i], parseInt(i, 10))
     }
-    return array;
-}
-;
-console.log(twoSum([2, 7, 11, 15], 9));
+    return array
+  };
+  console.log(twoSum([2,7,11,15], 9))
