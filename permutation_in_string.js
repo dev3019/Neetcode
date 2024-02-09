44bdfc6f8af21d324906050b01c31dddae8ca684
Divyansh Tangri <divyansh@wynd.network>
2024-02-09 00:51:59 -0800

    working solution, but high runtime

--- permutation_in_string.js
+++ permutation_in_string.js
@@ -1,38 +1,26 @@
-// function permute(str){
-  
-// }
-String.prototype.permute = function(){
-  const str = this
-  if(str.length<=1) return str
-  const result=[]
-  for(let i=0;i<str.length;i++){
-    const currChar = str[i]
-    const remainChars = str.slice(0, i)+str.slice(i+1)
-    const permutationsOfRemainChars = remainChars.permute(); // Recursively get permutations
-    for (const perm of permutationsOfRemainChars) {
-      result.push(currChar + perm); // Concatenate current character with permutations of remaining characters
-    }
-  }
-  return result
-}
 /**
  * @param {string} s1
  * @param {string} s2
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    let windowStart=0, windowEnd=0, answer=''
-    const combi=s1.permute()
-    while(windowStart<s2.length){
-      answer+=s2[windowStart]
-      if(answer.length===s1.length){
-        if(combi.includes(answer))return true
-        answer=answer.slice(1)
-      } 
-      windowStart++
+    const map = new Map()
+    for(const c of s1){
+      map.set(c, (map.get(c)||0)+1)
+    }
+
+    for(let i=0;i<=s2.length-s1.length;i++){
+      const tempMap = new Map(map)
+      let j=0
+      for(j=i;j<i+s1.length;j++){
+        if(tempMap.has(s2[j]) && tempMap.get(s2[j])>0){
+          tempMap.set(s2[j], tempMap.get(s2[j])-1)
+        }else break
+      }
+      if(j===i+s1.length) return true
     }
     return false
 };
 
 console.log(checkInclusion('ab','eidbaooo'))
-console.log(checkInclusion('ab','eidoaboo'))
+console.log(checkInclusion('ab','eidaoboo'))
