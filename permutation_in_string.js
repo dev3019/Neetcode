eb9f0ef2a11bef4011febf1620b5d135e0ce2b90
Divyansh Tangri <divyansh@wynd.network>
2024-02-09 01:34:16 -0800

    .

--- permutation_in_string.js
+++ permutation_in_string.js
@@ -4,33 +4,21 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-  const s1Len = s1.length
-  const s2Len = s2.length
+  const charFrequency = Array(26).fill(0); // Array to record char frequency
+  const aCharCode = 'a'.charCodeAt(0); // Default value to be used
 
-  const charFrequency = new Array(26).fill(0) //define array to record char frequency
-  const defaultCharVal = 'a'.charCodeAt(0)  //default value to be used
-  
-  //record char frequency from s1
-  for(const c of s1){
-    charFrequency[(c.charCodeAt(0)-defaultCharVal)]++
-  }
-  
-  for(let left=0, right=0; right<s2Len;right++){
-    if(right>=s1Len){
-      const leftChar = s2.charCodeAt(left)-defaultCharVal
-      console.log('left-before', charFrequency[leftChar], s2[left])
-      charFrequency[leftChar]++
-      console.log('left-after', charFrequency[leftChar], s2[left])
-      left++
+  for (const c of s1) charFrequency[c.charCodeAt(0) - aCharCode]++;
+
+  let left = 0, right = 0;
+  while (right < s2.length) {
+    charFrequency[s2.charCodeAt(right) - aCharCode]--;
+    while (charFrequency.every(a => a <= 0)) {
+      if (right - left + 1 === s1.length) return true;
+      charFrequency[s2.charCodeAt(left++) - aCharCode]++;
     }
-    const rightChar = s2.charCodeAt(right)-defaultCharVal
-    console.log('right-before', charFrequency[rightChar], s2[right])
-    charFrequency[rightChar]--
-    console.log('right-after', charFrequency[rightChar], s2[right])
-
-    if(!charFrequency.some((a)=>a!==0)) return true
+    right++;
   }
-  return false
+  return false;
 
 };
 
