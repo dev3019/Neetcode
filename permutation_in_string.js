2aec27d94f65dda730d707e114eda4618932b6f8
Divyansh Tangri <divyansh@wynd.network>
2024-02-09 01:48:05 -0800

    .

--- permutation_in_string.js
+++ permutation_in_string.js
@@ -7,14 +7,18 @@ var checkInclusion = function(s1, s2) {
   const charFrequency = Array(26).fill(0); // Array to record char frequency
   const aCharCode = 'a'.charCodeAt(0); // Default value to be used
 
-  for (const c of s1) charFrequency[c.charCodeAt(0) - aCharCode]++;
+  for (const c of s1) charFrequency[c.charCodeAt(0) - aCharCode]++; // record frequency of chars from s1
 
   let left = 0, right = 0;
   while (right < s2.length) {
+    console.log('right-before', charFrequency[s2.charCodeAt(right) - aCharCode], s2[right])
     charFrequency[s2.charCodeAt(right) - aCharCode]--;
+    console.log('right-after', charFrequency[s2.charCodeAt(right) - aCharCode], s2[right])
     while (charFrequency.every(a => a <= 0)) {
       if (right - left + 1 === s1.length) return true;
+      console.log('left-before', charFrequency[s2.charCodeAt(left) - aCharCode], s2[left])
       charFrequency[s2.charCodeAt(left++) - aCharCode]++;
+      console.log('left-after', charFrequency[s2.charCodeAt(left-1) - aCharCode], s2[left-1])
     }
     right++;
   }
