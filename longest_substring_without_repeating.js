4b89a4cef540ede9b306c8c1d67b916be4e70725
Divyansh Tangri <divyansh@wynd.network>
2024-02-24 00:12:59 -0800

    optimised

--- longest_substring_without_repeating.js
+++ longest_substring_without_repeating.js
@@ -2,29 +2,33 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  let answer=''
-  let windowStart=0
-  answer+=s[windowStart]
-  const set = new Set()
-  set.add(answer)
-  let temp=0
-  for(let i=1;i<s.length;i++){
-    if (!set.has(s[i])){
-      answer+=s[i]
-      set.add(s[i])
-    }else{
-      if(answer.length>temp) temp=answer.length
-      set.clear(answer[windowStart])
-      i=windowStart
-      windowStart++
-      answer=''
+var lengthOfLongestSubstring = function (s) {
+  let maxLength = 0;
+  let windowStart = 0;
+  const charIndexMap = new Map()
+
+  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+    const currentChar = s[windowEnd];
+
+    // If the current character is seen before, move the window start
+    // to the position after the last occurrence of the current character
+    if (
+      charIndexMap.has(currentChar) &&
+      charIndexMap.get(currentChar) >= windowStart
+    ) {
+      windowStart = charIndexMap.get(currentChar) + 1;
     }
+
+    // Update the index of the current character
+    charIndexMap.set(currentChar, windowEnd);
+
+    // Update the maximum length of the substring
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
   }
-  if(answer.length>temp) temp=answer.length
-  return temp
+
+  return maxLength;
 };
 
-console.log(lengthOfLongestSubstring("abcabcbb"))
-console.log(lengthOfLongestSubstring("bbbbb"))
-console.log(lengthOfLongestSubstring("pwwkew"))
+console.log(lengthOfLongestSubstring("abcabcbbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
